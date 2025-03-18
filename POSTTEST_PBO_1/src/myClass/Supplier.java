ced6ca3f6f0f442378c47872697772e3f4341e76
Dimas Raditya Permana <144466230+dimasradityapermana@users.noreply.github.com>
2025-03-18 00:00:38 -0700

    Add files via upload

---
+++ POSTTEST_PBO_1/src/myClass/Supplier.java
@@ -0,0 +1,48 @@
+package myClass;
+
+public class Supplier {
+    private final int id;
+    private String nama;
+    private String alamat;
+    private String telepon;
+
+    public Supplier(int id, String nama, String alamat, String telepon) {
+        this.id = id;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.telepon = telepon;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + " | nama: " + nama + " | alamat: " + alamat;
+    }
+}
