6bed2deaf8c4761c3554a5819e3a0a044a6d8a1a
Dimas Raditya Permana <144466230+dimasradityapermana@users.noreply.github.com>
2025-04-21 04:57:02 -0700

    Add files via upload

---
+++ POSTTEST_PBO_5/src/myClass/Pulpen.java
@@ -0,0 +1,28 @@
+package myClass;
+
+public class Pulpen extends Barang {
+    private boolean refillable;
+
+    public Pulpen(int id, String nama, int stok, double harga, boolean refillable) {
+        super(id, nama, stok, harga);
+        this.refillable = refillable;
+    }
+
+    public boolean isRefillable() {
+        return refillable;
+    }
+
+    public void setRefillable(boolean refillable) {
+        this.refillable = refillable;
+    }
+
+    @Override
+    public String getDeskripsi() {
+        return "Pulpen (refillable: " + refillable + ")";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Refillable: " + refillable;
+    }
+}
